a8d133e29fd520ef501944614a080d458cf9c522
hiro-lapis <coolish555ddddd@gmail.com>
2023-10-14 15:30:44 +0900

    add typescript, tailwind

--- app.vue
+++ app.vue
@@ -1,5 +1,22 @@
 <template>
   <div>
+    <div class="flex justify-center">
+      <button
+        @click="increment"
+      >
+        {{ count }}
+      </button>
+    </div>
     <NuxtWelcome />
   </div>
 </template>
+
+<script lang="ts" setup>
+console.log('Hello World')
+
+const {
+  count,
+  increment,
+} = useCount()
+
+</script>
