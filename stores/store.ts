082979da75ba1682e5d01fa139ebe96b51f008b4
hiro-lapis <coolish555ddddd@gmail.com>
2024-04-07 23:22:11 +0900

    add practice writing page

--- stores/store.ts
+++ stores/store.ts
@@ -4,8 +4,7 @@ export const useStore = defineStore(
   'main',
   () => {
     const count = ref(3)
-    // const count = ref(3)
-    const getCount = computed(() => count.value)
+    const getCount = computed<number>(() => count.value)
     const doubleCount = computed(() => count.value * 2)
     const increment = () => {
       count.value += 1
@@ -14,7 +13,27 @@ export const useStore = defineStore(
       count.value -= 1
     }
 
-    return { count, getCount, doubleCount, increment, decrement }
+    // text
+    const text = ref('')
+    const setText = (newText: string) => text.value = newText
+    const getText = computed(() => text.value)
+    const autoSaveText = ref(true)
+    const changeAutoSaveText = () => autoSaveText.value = !autoSaveText.value
+    const enabledAutoSaveText = computed(() => autoSaveText.value)
+
+    return {
+      count,
+      getCount,
+      doubleCount,
+      increment,
+      decrement,
+      text,
+      setText,
+      getText,
+      autoSaveText,
+      enabledAutoSaveText,
+      changeAutoSaveText,
+     }
   },
   // https://prazdevs.github.io/pinia-plugin-persistedstate/guide/#usage
   {
