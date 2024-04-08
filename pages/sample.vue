8632eb7d0c96c3cf3cdea48ef0b4f35a185ad153
hiro-lapis <coolish555ddddd@gmail.com>
2024-04-08 23:00:06 +0900

    add sample page

---
+++ pages/sample.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <UInput :id='uInput' v-model="value" />
+    <UButton
+      :id='uButton'
+      @click=""
+      size="md"
+    >スタート</UButton>
+    <NuxtWelcome />
+  </div>
+</template>
+
+<script lang="ts" setup>
+console.log('Hello World')
+
+const value = ref('')
+const uInput = useId()
+const uButton = useId()
+</script>
