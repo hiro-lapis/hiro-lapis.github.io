7992a484f3b536e78639949d8f7344334be21d3a
hiro-lapis <coolish555ddddd@gmail.com>
2024-04-16 09:46:42 +0900

    replace next ui textarea

--- app/writings/page.tsx
+++ app/writings/page.tsx
@@ -1,7 +1,27 @@
-import React from 'react'
+'use client'
+import { useState, useMemo } from 'react'
+import { Textarea } from '@nextui-org/react'
 
 const page = () => {
-  return <div>writing</div>
+  const [text, setText] = useState('')
+  const wordCount = useMemo(() => {
+    return text.split(/\s+/).filter(Boolean).length
+  }, [text]) // textが更新されるたびに再計算
+
+  return (
+    <div className="w-96 mx-auto">
+      <div className="my-auto wid">
+        <p className="mb-1">wordcount: {wordCount} </p>
+        <Textarea
+          value={text}
+          onChange={(e) => setText(e.target.value)}
+          label='each letter feeds on your future...'
+          color='success'
+          className="max-w-s my-auto"
+        />
+      </div>
+    </div>
+  )
 }
 
 export default page
