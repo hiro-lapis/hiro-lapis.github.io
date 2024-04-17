07d91bf8ed69d7238b5a5a2dc4864f03bbc007a9
hiro-lapis <coolish555ddddd@gmail.com>
2024-04-17 23:23:34 +0900

    add link to writings

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,5 @@
 import Image from 'next/image'
+import { Link } from '@nextui-org/react'
 
 export default function Home() {
   return (
@@ -107,6 +108,7 @@ export default function Home() {
             Instantly deploy your Next.js site to a shareable URL with Vercel.
           </p>
         </a>
+        <Link href="hiro-lapis.github.io/writings">writings</Link>
       </div>
     </main>
   )
