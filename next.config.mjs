0427cc3b83a0ca569678b79b0ce50bdee4299eea
hiro-lapis <coolish555ddddd@gmail.com>
2024-04-18 09:52:08 +0900

    add comment

--- next.config.mjs
+++ next.config.mjs
@@ -8,6 +8,7 @@ const nextConfig = {
     output: 'export',
 
     /**
+     * [NOTE]: Deploy with gitHub actions, you dont need set this. A workflow resolve basePath automatically
      * Set base path. This is the slug of your GitHub repository.
      *
      * @see https://nextjs.org/docs/app/api-reference/next-config-js/basePath
